05e44e1503b841b9f60461b5360f491c70300eb9
Tanirika <tanirikarangarajan@gmail.com>
2026-07-14 17:44:12 +0530

    Integrate Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 
@@ -23,6 +24,7 @@ function App() {
         <Demo />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
